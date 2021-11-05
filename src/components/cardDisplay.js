ba092d34f9d3735dc0b5136639a9d64aadc749e8
jackjk521 <jedchu541@gmail.com>
2021-11-05 23:12:26 +0800

    tried to do the grid system but failed, code still there

--- src/components/cardDisplay.js
+++ src/components/cardDisplay.js
@@ -1,21 +1,42 @@
 import React from 'react';
 import Card from "./card";
+// import GridSystem from "./GridSystem"
+
+
 
 const cardDisplay = ({assets}) => {
  
         return (
-            <div class="card" style={{width: "18rem"}}>
+            <div class="cardBody">
 
                 {
                     (assets !== 0) ?
                         assets.map(asset =>{
                             return(
                                <Card asset = {asset}/>
-                            // <img src = {asset.image_url}/>
                             )
                     }) : null
                 }
           </div>
+
+        // <div className='App'>
+        // {/* colCount is the number of columns for our grid system.
+        //     md is a bootstrap breakpoint (will discuss breakpoints in the article)
+        // */}
+        // <GridSystem colCount={2} md={6}>
+        // {/* Here we are mapping every element to an <Item /> and pass props.
+        //     map returns an array of JSX that the grid system will take as children.
+        // */}
+        //         {
+        //             (assets !== 0) ?
+        //                 assets.map(asset =>{
+        //                     return(
+        //                        <Card  key = {asset.id} asset = {asset}/>
+        //                     )
+        //             }) : null
+        //         }
+        // </GridSystem>
+        // </div>
         );
 }
 export default cardDisplay;
